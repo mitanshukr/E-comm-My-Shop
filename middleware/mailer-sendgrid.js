1210f71f7e1dd9ffae97fd26b2dc4292a55de902
Mitanshu Kumar <mitanshukr@yahoo.com>
2021-07-28 12:42:13 +0530

    Update mailer-sendgrid.js

--- middleware/mailer-sendgrid.js
+++ middleware/mailer-sendgrid.js
@@ -3,7 +3,7 @@ const sendgridTransport = require('nodemailer-sendgrid-transport');
 
 const transport = nodemailer.createTransport(sendgridTransport({
     auth: {
-        api_key: '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'
+        api_key: '*****************************************'
     }
 }));
 
